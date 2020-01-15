75a4790f8a75355d9389aee1f73118e9baa988e0
Florian Wechsung <f.wechsung@gmail.com>
2020-01-15 13:53:55 -0500

    jobs

---
+++ jobs.sh
@@ -0,0 +1,6 @@
+python3 ridgalgo.py 1 3 100 2 5 2>&1 | tee 3-1.txt
+python3 ridgalgo.py 2 3 100 2 5 2>&1 | tee 3-2.txt
+python3 ridgalgo.py 1 4 100 2 5 2>&1 | tee 4-1.txt
+python3 ridgalgo.py 2 4 100 2 5 2>&1 | tee 4-2.txt
+python3 ridgalgo.py 1 5 100 2 5 2>&1 | tee 5-1.txt
+python3 ridgalgo.py 2 5 100 2 5 2>&1 | tee 5-2.txt
